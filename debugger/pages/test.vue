4e953d0a0f3f70d79e54df3b191aa2c1e2040147
feipe <wfpaisa@gmail.com>
2019-05-04 11:54:25 -0500

    add debugger

---
+++ debugger/pages/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Test -{{ contador }}</h1>
+</template>
+<script>
+const cuenta = 11
+export default {
+  data: function() {
+    return {
+      contador: cuenta
+    }
+  }
+}
+</script>
