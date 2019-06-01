adb079adf8266086961b3737f7ae4db65f7a9e17
feipe <wfpaisa@gmail.com>
2019-06-01 17:11:44 -0500

    Base estructure

--- base-v2/plugins/vuetify.js
+++ base-v2/plugins/vuetify.js
@@ -4,12 +4,13 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: colors.blue.darken2,
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
+    primary: '#001F4C', // rgb(0,31,76)
+    // accent: colors.grey.darken3,
+    secondary: '#de317a',
+    // secondary: colors.amber.darken3,
+    info: colors.blue.base,
     warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
+    error: colors.pink.base,
     success: colors.green.accent3
   }
 })
